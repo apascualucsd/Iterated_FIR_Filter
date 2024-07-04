a12618a14f25010ca5d74b496c86b49de88ebefb
Alexis Pascual <102634499+apascualucsd@users.noreply.github.com>
2024-07-04 13:40:16 -0700

    Update FIR_interpolated2_0_packed.m

--- FIR_interpolated2_0_packed.m
+++ FIR_interpolated2_0_packed.m
@@ -1,5 +1,4 @@
-fs1 = 50;
-fs2 = 200;
+fs1 = 50; fs2 = 100; fs3 = 200;
 f1 = 8; 
 f2 = 12; 
 A_dB = 80;
@@ -19,42 +18,62 @@ h(MM+1)=1; %correct failed 0/0 computation
 h0=h.*kaiser(2*MM+1,Beta)'; %Apply window to obtain Stopband Ripple
 h2=h0*(f2+f1)/fs1; %Scale filter gain f0/fs
 
-hh3 = zeros(1,4*length(h2));
-hh3(1:4:end) = h2;
+h3 = zeros(1,2*length(h2));
+h3(1:2:end) = h2;
 
-r2 = 0.01;
-dev2 = [(10^(r2/20)-1)/(10^(r2/20)+1) 10^(-A_dB/20)]; 
-[n_g2, fo_g2, ao_g2, w_g2] = firpmord([fs2/8 fs2/4], [1 0], dev2, 2*fs2);
-gg1 = firpm(n_g2, fo_g2, ao_g2, w_g2);
+f13 = fs2/8;
+f23 = fs2/4;
 
-figure(211)
+phi3 = 2*pi*(-length(h3):length(h3))*(f13+f23)/(2*fs2);
+h1 = sin(phi3)./phi3;
+h1(length(h3)+1) = 1;
+h03 = h1 .* kaiser(2*length(h3)+1, Beta)';
+g1 = h03*(f23+f13)/fs2;
+
+h4 = conv(h3, g1,'same');
+h5 = zeros(1,2*length(h4));
+h5(1:2:end) = h4;
+
+f14 = fs3/8;
+f24 = fs3/4;
+
+phi4 = 2*pi*(-length(h5):length(h5))*(f14+f24)/(2*fs3);
+h7 = sin(phi4)./phi4;
+h7(length(h5)+1) = 1;
+h04 = h7 .* kaiser(2*length(h5)+1, Beta)';
+g2 = h04*(f24+f14)/fs3;
+
+h6 = conv(h5,g2,'same');
+
+figure(206)
 subplot(3,1,1)
+plot(h6,'b','linewidth',2)
 grid on
-plot(hh3,'b','linewidth',2)
 axis([-1 50 -0.1 0.5])
-title('Impulse Response, Kaiser')
+title('Impulse Response, lowpass interpolating filter ')
 xlabel('Time Index')
 ylabel('Amplitude')
 
-fh=fftshift(20*log10(abs(fft(hh3,1024))));
-fh2=fftshift(20*log10(abs(fft(gg1,1024))));
+fh3=fftshift(20*log10(abs(fft(g2,1024))));
+
+fh2=fftshift(20*log10(abs(fft(h6,1024))));
 
 subplot(3,1,2)
-plot([-0.5:1/1024:0.5-1/1024]*fs2,fh,'b','linewidth',2)
+plot([-0.5:1/1024:0.5-1/1024]*fs3,fh2,'b','linewidth',2)
 hold on
-plot([-0.5:1/1024:0.5-1/1024]*fs2,fh2,'r','linewidth',2)
+plot([-0.5:1/1024:0.5-1/1024]*fs3,fh3,'--r','linewidth',2)
 hold off
 grid on
 axis([-20 +20 -120 10])
-title('Frequency Response of hh3 and gg1')
+title('Frequency Response of g2 and h6')
 xlabel('Frequency, (kHz)')
 ylabel('Log Mag (dB)')
 
 subplot(3,2,5)
-plot([-0.5:1/1024:0.5-1/1024]*fs2,fh,'b','linewidth',2)
+plot([-0.5:1/1024:0.5-1/1024]*fs3,fh2,'b','linewidth',2)
 grid on
 hold on 
-plot([-0.5:1/1024:0.5-1/1024]*fs2,fh2,'r','linewidth',2)
+plot([-0.5:1/1024:0.5-1/1024]*fs3,fh3,'r','linewidth',2)
 hold off
 axis([-10 +10 -0.005 0.005])
 title('In-Band Ripple Detail')
@@ -62,7 +81,7 @@ xlabel('Frequency, (kHz)')
 ylabel('Log Mag (dB)')
 
 subplot(3,2,6)
-plot([-0.5:1/1024:0.5-1/1024]*fs2,fh,'b','linewidth',2)
+plot([-0.5:1/1024:0.5-1/1024]*fs3,fh2,'b','linewidth',2)
 grid on
 axis([10 +30 -120 10])
 title('Transition BW Detail')
